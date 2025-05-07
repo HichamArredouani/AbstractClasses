e564ab4e804b927c1d825418f3fe8780c0995005
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-07 19:30:47 +0200

    Mainnnnnnnnn

---
+++ src/Duck.java
@@ -0,0 +1,12 @@
+class Duck implements Flyable, Swimmable {
+        @Override
+                public void fly() {
+            System.out.println("Duck is flying. ");
+        }
+
+        @Override
+                public void swim() {
+            System.out.println("Duck is swimming. ");
+        }
+    }
+}
---
+++ src/Mainnnnnnnnnnn.java
@@ -0,0 +1,9 @@
+public class Mainnnnnnnnnnn {
+    public static void main(String[] args) {
+        Duck blue = new Duck();
+        blue.fly();
+        blue.swim();
+
+    }
+
+}
